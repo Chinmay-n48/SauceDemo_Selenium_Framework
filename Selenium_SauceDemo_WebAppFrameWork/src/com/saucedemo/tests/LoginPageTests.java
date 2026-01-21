102dbef0a0760494fe44ab05646bbd0f84b767a0
Chinmay-n48 <chinmaynale01@gmail.com>
2026-01-21 07:27:32 +0530

    Added the public void TC_Login_015() to public void TC_Login_019()

--- Selenium_SauceDemo_WebAppFrameWork/src/com/saucedemo/tests/LoginPageTests.java
+++ Selenium_SauceDemo_WebAppFrameWork/src/com/saucedemo/tests/LoginPageTests.java
@@ -2,6 +2,7 @@ package com.saucedemo.tests;
 import java.sql.Time;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -22,7 +23,7 @@ public class LoginPageTests extends BaseTest {
 	    }
 	
 	@Test
-	//Valid Login
+	//Verify login with valid credentials
 	public void TC_LoginPage_001()  {
 		login.login("standard_user","secret_sauce");
 		Assert.assertTrue(driver.getCurrentUrl().contains("inventory"),"Login failed for problem user");
@@ -30,7 +31,7 @@ public class LoginPageTests extends BaseTest {
 		Assert.assertEquals(driver.findElement(By.xpath("//span[@class='title']")).getText(), "Products" , "Product title is not displayed");
 	}
 	@Test
-	//Login using Enter key
+	//Verify login via keyboard
 		public void TC_LoginPage_002() {
 			driver.findElement(login.UserNameField).sendKeys("standard_user");
 	        driver.findElement(login.PasswordField).sendKeys("secret_sauce");
@@ -39,56 +40,56 @@ public class LoginPageTests extends BaseTest {
 			Assert.assertEquals(driver.findElement(By.xpath("//span[@class='title']")).getText(), "Products" , "Product title is not displayed");
 		}
 	@Test
-	//Invalid Username
+	//Verify error for invalid username
 		public void TC_LoginPage_003() {
 		     login.login("standarduser","secret_sauce");
 		     Assert.assertEquals(login.getLoginErrorMessagedisplayed(), true, "Error Message not displayed");
 		     Assert.assertEquals(login.getLoginErrorMessage(),"Epic sadface: Username and password do not match any user in this service", "Error message is not matched");
 		}
 	@Test
-	//Invalid Password
+	//Verify error for invalid password
 	public void TC_LoginPage_004()  {
 		login.login("standard_user","secretsauce");
 		Assert.assertEquals(login.getLoginErrorMessagedisplayed(), true, "Error Message not displayed");
 	     Assert.assertEquals(login.getLoginErrorMessage(),"Epic sadface: Username and password do not match any user in this service", "Error message is not matched");
 	}
 	@Test
-	//Invalid Username and Password
+	//Verify error for invalid credentials
 	public void TC_LoginPage_005()  {
 		login.login("standarduser","secretsauce");
 		Assert.assertEquals(login.getLoginErrorMessagedisplayed(), true, "Error Message not displayed");
 	     Assert.assertEquals(login.getLoginErrorMessage(),"Epic sadface: Username and password do not match any user in this service", "Error message is not matched");
 	}
 	@Test
-	//Blank Username and Password
+	//Verify validation for blank fields
 	public void TC_LoginPage_006()  {
 		login.login("","");
 		Assert.assertEquals(login.getLoginErrorMessagedisplayed(), true, "Error Message not displayed");
 	     Assert.assertEquals(login.getLoginErrorMessage(),"Epic sadface: Username is required", "Error message is not matched");
 	}
 	@Test
-	//Blank Username
+	//Verify username mandatory validation
 	public void TC_LoginPage_007()  {
 		login.login("","secret_sauce");
 		Assert.assertEquals(login.getLoginErrorMessagedisplayed(), true, "Error Message not displayed");
 		Assert.assertEquals(login.getLoginErrorMessage(), "Epic sadface: Username is required", "Error message is not matched");
 	}
 	@Test
-	//Blank Password
+	//Verify password mandatory validation
 	public void TC_LoginPage_008() {
 		login.login("standard_user", "");
 		Assert.assertEquals(login.getLoginErrorMessagedisplayed(), true, "Error Message not displayed");
 		Assert.assertEquals(login.getLoginErrorMessage(), "Epic sadface: Password is required", "Error message is not matched");		
 	}
 	@Test
-	//Locked Out User
+	//Verify locked user restriction
 	public void TC_LoginPage_009() {
 		login.login("locked_out_user", "secret_sauce");
 		Assert.assertEquals(login.getLoginErrorMessagedisplayed(), true, "Error Message not displayed");
 		Assert.assertEquals(login.getLoginErrorMessage(), "Epic sadface: Sorry, this user has been locked out.","Error message is not matched");
 	}
 	@Test
-	//Problem User Login
+	//Verify problem user login
 	public void TC_LoginPage_010() {
 		login.login("problem_user", "secret_sauce");
 		Assert.assertTrue(driver.getCurrentUrl().contains("inventory"), "Login failed for problem user");
@@ -96,7 +97,7 @@ public class LoginPageTests extends BaseTest {
 		Assert.assertEquals(imageElement.getAttribute("src"), "https://www.saucedemo.com/static/media/sl-404.168b1cce10384b857a6f.jpg", "The broken image is not displayed");
 	}
 	@Test
-	//Performance Glitch User
+	//Verify delayed login
 	public void TC_LoginPage_011() {
 		long startTime  = System.nanoTime();
 		login.login("performance_glitch_user", "secret_sauce");
@@ -104,10 +105,10 @@ public class LoginPageTests extends BaseTest {
 	    long durationNano = endTime - startTime;
 	    long durationMillis = durationNano / 1_000_000;
 	    Assert.assertTrue(durationMillis>5000,"Login in the lessa than 5 sec ");;
-		Assert.assertTrue(driver.getCurrentUrl().contains("inventory"), "Login failed for problem user");				
+		Assert.assertTrue(driver.getCurrentUrl().contains("inventory"), "Login failed for user");				
 	}
 	@Test
-	//Verify UI Elements
+	//Verify login page UI
 	public void TC_Login_012() {
 		Assert.assertTrue(login.getLoginPageTitledisplayed(), "Login Page title not displayed");
 		Assert.assertEquals(login.getLoginPageTitleText(), "Swag Labs", "Login page title not matched");
@@ -125,18 +126,71 @@ public class LoginPageTests extends BaseTest {
 		
 	}
 	@Test
-	//Password Masking
+	//Verify password masking
 	public void TC_Login_013() {
 		driver.findElement(login.PasswordField).sendKeys("secret_sauce");
 		Assert.assertEquals(login.getPasswordFieldMasking(), "password", "Password not Masking");
 	}
 	@Test
-	//Close Error Message
+	//Verify error close icon
 	public void TC_Login_014() {
 		login.login("standarduser", "secretsauce");
 		Assert.assertTrue(login.getLoginErrorMessagedisplayed(), "Error Message not displayed");
 		driver.findElement(login.ErrorMessageCloseBtn).click();
 		
 	}
-	
+	@Test
+	//Verify error on retry
+	public void TC_Login_015() {
+		login.login("standarduser", "secretsauce");
+		Assert.assertTrue(login.getLoginErrorMessagedisplayed(), "Error Message not displayed");
+		driver.findElement(login.LoginBtn).click();
+		Assert.assertTrue(login.getLoginErrorMessagedisplayed(), "Error Message not displayed");
+	}
+	@Test
+	//Verify session on refresh
+	public void TC_Login_016() {
+		login.login("standard_user", "secret_sauce");
+		Assert.assertTrue(driver.getCurrentUrl().contains("inventory"), "Login failed for user");
+		Assert.assertEquals(driver.findElement(By.xpath("//span[@class='title']")).getText(), "Products" , "Product title is not displayed");
+		driver.navigate().refresh();
+		Assert.assertTrue(driver.getCurrentUrl().contains("inventory"), "Login failed for user");
+		Assert.assertEquals(driver.findElement(By.xpath("//span[@class='title']")).getText(), "Products" , "Product title is not displayed");		
+	}
+	@Test
+	//Verify session security
+	public void TC_Login_017() {
+		login.login("standard_user", "secret_sauce");
+		Assert.assertTrue(driver.getCurrentUrl().contains("inventory"), "Login failed for user");
+		Assert.assertEquals(driver.findElement(By.xpath("//span[@class='title']")).getText(), "Products" , "Product title is not displayed");
+		driver.navigate().back();
+		Assert.assertTrue(driver.getCurrentUrl().contains("https://www.saucedemo.com/"),"User was not redirected to login page");
+		Assert.assertTrue(login.getLoginPageTitledisplayed(), "Login Page title not displayed");
+	}
+	@Test
+	//Verify access without login
+	public void TC_Login_018() throws InterruptedException {
+		driver.get("https://www.saucedemo.com/inventory.html");
+		Thread.sleep(3000);
+		Assert.assertTrue(driver.getCurrentUrl().contains("https://www.saucedemo.com/"), "User was not redirected to login page");
+		Assert.assertTrue(login.getLoginErrorMessagedisplayed(),"Error message not displayed");
+		Assert.assertEquals(login.getLoginErrorMessage(),"Epic sadface: You can only access '/inventory.html' when you are logged in.","Error Message not displayed");
+	}
+	@Test
+	//Verify login on multiple browsers
+	public void TC_Login_019() {
+		login.login("standard_user", "secret_sauce");
+		
+		// Desktop
+	    driver.manage().window().setSize(new Dimension(1920, 1080));
+	    Assert.assertTrue(driver.findElement(By.className("inventory_list")).isDisplayed());
+
+	    // Tablet
+	    driver.manage().window().setSize(new Dimension(768, 1024));
+	    Assert.assertTrue(driver.findElement(By.id("react-burger-menu-btn")).isDisplayed());
+	    
+	 // Mobile
+	    driver.manage().window().setSize(new Dimension(375, 667));
+	    driver.findElement(By.id("react-burger-menu-btn")).click();		
+	}
 }
