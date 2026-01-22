c2be4a6d83494d7054671970685202691382d26e
CNale <CNale@LPT3-CNALE.frsymtrax.local>
2026-01-22 17:01:28 +0530

    Added the HomePage and HomePageTest class.

---
+++ Selenium_SauceDemo_WebAppFrameWork/src/com/saucedemo/pages/HomePage.java
@@ -0,0 +1,12 @@
+package com.saucedemo.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+	WebDriver driver;
+	
+	public HomePage(WebDriver driver) {
+        this.driver = driver;
+    } 
+
+}
