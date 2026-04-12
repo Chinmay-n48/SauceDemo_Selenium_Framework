a3441cae1ee96c67fc7ab8b3826673d5da8169e3
Chinmay-n48 <chinmaynale01@gmail.com>
2026-04-12 17:00:55 +0530

    commit the new code

--- Selenium_SauceDemo_WebAppFrameWork/src/com/saucedemo/utils/ActionUtils.java
+++ Selenium_SauceDemo_WebAppFrameWork/src/com/saucedemo/utils/ActionUtils.java
@@ -56,6 +56,5 @@ public class ActionUtils {
 	        return "Null";
 	    }
 	}
-	
-		
+			
 	}
--- Selenium_SauceDemo_WebAppFrameWork/src/com/saucedemo/utils/WaitUtils.java
+++ Selenium_SauceDemo_WebAppFrameWork/src/com/saucedemo/utils/WaitUtils.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -11,8 +12,10 @@ public class WaitUtils {
 	WebDriver driver;
 	
 	public static void waitForPresence(WebDriver driver, By locator) {
-        new WebDriverWait(driver, Duration.ofSeconds(10))
-                .until(ExpectedConditions.presenceOfElementLocated(locator));
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.presenceOfElementLocated(locator));
     }
-
+	
+	public static void waitForEnabled(WebDriver driver,WebElement element ) {
+		new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(element));
+	}
 }
